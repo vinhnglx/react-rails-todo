8a17a36a42b89ece6f8568b8798247312aeec937
Vinh Nguyen <vinh@betacode.io>
2016-08-23 22:21:29 +0800

    Define React route for betahouse-front-end app

--- betahouse-front-end/src/index.js
+++ betahouse-front-end/src/index.js
@@ -1,7 +1,21 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router'
 import App from './components/Main';
+import Index from './components/Index';
 
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('application'));
+// ReactDOM.render(<App />, document.getElementById('application'));
+
+let reactNode = document.getElementById('application');
+
+if (reactNode) {
+  ReactDOM.render(
+    <Router history={browserHistory}>
+      <Route component={App}>
+        <Route path="/" component={Index} />
+      </Route>
+    </Router>
+  , reactNode)
+}
