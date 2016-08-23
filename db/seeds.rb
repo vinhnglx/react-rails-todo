0a8ae480653298ebc9e1244f64f2405a94c7e96a
Vinh Nguyen <vinh@betacode.io>
2016-08-23 21:44:07 +0800

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+u1 = User.create(email: "foo@bar.com", password: "abcd1234")
+u2 = User.create(email: "john@example.com", password: "abcd1234")
+
+10.times do |i|
+  Post.create!(user: u1)
+end
