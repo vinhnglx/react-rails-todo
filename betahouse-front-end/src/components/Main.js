adcf63f615e071f1b02db9e4e4061520e599f64e
Vinh Nguyen <vinh@betacode.io>
2016-08-23 19:53:41 +0800

    Add betahouse-front-end code skeleton

---
+++ betahouse-front-end/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="index">
+        <h2> Hello world </h2>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
