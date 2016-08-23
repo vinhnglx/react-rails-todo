045e2ac2b32a4e5a9745b3c0c38d06005b0b8cac
Vinh Nguyen <vinh@betacode.io>
2016-08-23 22:22:43 +0800

    Define Index component to listing all posts

---
+++ betahouse-front-end/src/components/Index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>List posts</h2>
+      </div>
+    )
+  }
+}
