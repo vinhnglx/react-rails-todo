cc15c79e61d0c6f9daed4bfae416f6c548a8ebc5
Vinh Nguyen <vinh@betacode.io>
2016-08-25 00:27:58 +0800

    Remove h2

--- betahouse-front-end/src/components/Main.js
+++ betahouse-front-end/src/components/Main.js
@@ -4,7 +4,6 @@ class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <h2> Hello world </h2>
         {this.props.children}
       </div>
     );
