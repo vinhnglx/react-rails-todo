7104bcb0c5d59bc177c65176048fa51a27025084
Vinh Nguyen <vinh@betacode.io>
2016-08-25 00:26:17 +0800

    Implement Create and Delete post APIs

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -2,4 +2,18 @@ class V1::PostsController < V1::BaseController
   def index
     render json: Post.all, status: :ok
   end
+
+  def create
+    post = Post.create(content: params[:body])
+
+    render json: post, status: :created
+  end
+
+  def destroy
+    post = Post.find_by(id: params[:id])
+    post.destroy
+
+    render json: Post.all, status: :ok
+  end
 end
+
