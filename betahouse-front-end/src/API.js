052c929f9bbd6ab8a392010765f51d7fc048ef93
Vinh Nguyen <vinh@betacode.io>
2016-08-25 00:27:30 +0800

    Use xmlHttp request to call Create Post and Delete Post APIs

--- betahouse-front-end/src/API.js
+++ betahouse-front-end/src/API.js
@@ -9,7 +9,7 @@ export default {
     let url = ActionTypes.HOST_NAME + "/v1/posts";
     let rawPosts;
 
-    xmlHttp.onreadystatechange = function() {
+    xmlHttp.onreadystatechange = () => {
       if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
         rawPosts = JSON.parse(xmlHttp.responseText);
         ServerActions.receivedPosts(rawPosts);
@@ -18,5 +18,40 @@ export default {
 
     xmlHttp.open("GET", url, true);
     xmlHttp.send();
+  },
+
+  createPost(body) {
+    let xmlHttp = new XMLHttpRequest();
+    let url = ActionTypes.HOST_NAME + "/v1/posts";
+    let params = "body=" + body;
+    let rawPost;
+
+    xmlHttp.onreadystatechange = () => {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 201) {
+        rawPost = JSON.parse(xmlHttp.responseText);
+        ServerActions.receivedPost(rawPost);
+      }
+    }
+
+    xmlHttp.open("POST", url, true);
+    xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlHttp.send(params);
+  },
+
+  deletePost(postId, postIndex) {
+    console.log('id', postId);
+    let xmlHttp = new XMLHttpRequest();
+    let url = ActionTypes.HOST_NAME + "/v1/posts/" + postId;
+
+    xmlHttp.onreadystatechange = () => {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        ServerActions.deletedPost(postIndex);
+      }
+    }
+
+    xmlHttp.open("DELETE", url, true);
+    xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlHttp.send();
+
   }
 }
