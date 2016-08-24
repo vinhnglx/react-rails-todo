fb03afac36b4da2597342d1aa752066567dc8559
Vinh Nguyen <vinh@betacode.io>
2016-08-25 00:27:43 +0800

    Call 2 APIs from PostActions

--- betahouse-front-end/src/actions/PostActions.js
+++ betahouse-front-end/src/actions/PostActions.js
@@ -3,5 +3,11 @@ import API from '../API';
 export default {
   getAllPosts() {
     API.getAllPosts();
+  },
+  createPost(body) {
+    API.createPost(body);
+  },
+  deletePost(id, index) {
+    API.deletePost(id, index);
   }
 }
